13e49e66a37b7ecb96c5441de6066cd7bfbdac29
xLeilu <71885335+xLeilu@users.noreply.github.com>
2024-06-11 21:01:10 +0200

    feat: implemented search on product (#24)

--- src/components/ProductsPanel/SideNav/CategoriesNav.js
+++ src/components/ProductsPanel/SideNav/CategoriesNav.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "../ProductsPanel.css";
 
-const CategoriesNav = () => {
+const CategoriesNav = ({ searchValue, setSearchValue }) => {
     return (
         <div className="productsPanelSideNav">
+            <input
+                type="text"
+                placeholder="Szukaj produktu"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+            />
             <div id="categoriesSideNav"></div>
         </div>
     );

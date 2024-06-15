93df44e64a8d1722bfbe69118ebcc8201ae1cdbc
Mikolaj Grabarski <mikolaj.grab1@gmail.com>
2024-06-15 09:29:23 +0200

    Get images from server

--- src/components/ProductsPanel/ProductItem.js
+++ src/components/ProductsPanel/ProductItem.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./ProductsPanel.css";
 import imgFile from "./placeholder.png";
 import { Link } from "react-router-dom";
+import ImageWithPlaceholder from "../ImageWithPlaceholder/ImageWithPlaceholder";
 
 const ProductItem = ({ product }) => {
+    const host = process.env.REACT_APP_API_BASE_URL;
+    const imgPath = `${host}/component/GetComponentImage/${product.componentId}`
     return (
         <Link
             to={`/produkt/${product.componentId}/${product.model}/${product.manufacturer}/${product.price}/${product.quantityAvailable}`}
             title={product.model}
         >
             <div id="product-item">
-                <img src={imgFile} alt={product.model} />
+                <ImageWithPlaceholder src={imgPath} alt={product.model} />
                 <h3>{product.manufacturer + " " + product.model}</h3>
                 <p>{product.price.toFixed(2)} zł</p>
             </div>
